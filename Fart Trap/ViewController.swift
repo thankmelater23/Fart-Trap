b2c9198d42478c227fee0c2f94df745261a2b3df
Andre Villanueva <thankmelater23@me.com>
2016-09-21 09:48:46 -0700

    Initial Commit

---
+++ Fart Trap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fart Trap
+//
+//  Created by Andre Villanueva on 9/21/16.
+//  Copyright © 2016 Bang Bang Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
